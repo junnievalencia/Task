4496d24cf0e1f85791b8e32119f4d885f6229bbd
junnievalencia <junnievalencia544@gmail.com>
2024-11-05 09:10:20 +0000

    128

---
+++ Tasksheet128.java
@@ -0,0 +1,67 @@
+interface Shape {
+    double calculateArea();
+    double calculatePerimeter();
+}
+
+abstract class AbstractShape implements Shape {
+    String color;
+
+    public AbstractShape(String color) {
+        this.color = color;
+    }
+}
+
+class Circle extends AbstractShape {
+    double radius;
+
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle extends AbstractShape {
+    double length, width;
+
+    public Rectangle(String color, double length, double width) {
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+public class Tasksheet128 {
+    public static void main(String[] args) {
+        Circle circle = new Circle("Red", 5);
+        System.out.println("Circle:");
+        System.out.println("Color: " + circle.color);
+        System.out.println("Area: " + circle.calculateArea());
+        System.out.println("Perimeter: " + circle.calculatePerimeter());
+
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        System.out.println("\nRectangle:");
+        System.out.println("Color: " + rectangle.color);
+        System.out.println("Area: " + rectangle.calculateArea());
+        System.out.println("Perimeter: " + rectangle.calculatePerimeter());
+    }
+}
